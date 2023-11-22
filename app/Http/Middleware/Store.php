5c5466781710602748a6f26879538eb493c47637
Mustafa Aly Maklad <mustafamaklad17@gmail.com>
2023-11-22 15:12:49 +0200

    fixed store middleware conditions

--- app/Http/Middleware/Store.php
+++ app/Http/Middleware/Store.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Store as StoreModel;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,14 +19,14 @@ class Store
         $user = $request->user();
         if (!$user || $user->role !== 'store') {
             return response()->json(['message' => 'Unauthorized'], 403);
-
-            if ($user->status === 'blocked') {
-                return response()->json(['message'=> 'Your account is currently blocked'], 403);
-            }
-            if ($user->status === 'pending') {
+        } else {
+            $status = StoreModel::where('user_id', $user->id)->value('status');
+            if ($status === 'pending') {
                 return response()->json(['message'=> 'Your account is currently pending, contact the admin to activate your account'], 403);
             }
-
+            if ($status === 'blocked') {
+                return response()->json(['message'=> 'Your account is currently blocked'], 403);
+            }
         }
         return $next($request);
     }
